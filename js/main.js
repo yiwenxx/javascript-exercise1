d5b61f3037c840ea96d5fb30968886e3ef447d75
yiwenxx <yiwenxx@users.noreply.github.com>
2022-04-06 16:54:05 +0000

    deploy: f9e5c101950c525d139df212c7b97a188840db2d

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
+var firstName = "Yiwen";
+var lastName = "Xu";
+var age = 21;
+var favoriteFood = "Dark Chocolote";
 
+console.log(
+  `My name is ${firstName}. I am ${age} years old and I love to eat ${favoriteFood}.`
+);
+
+var num = 2;
+var result = num + num;
+
+console.log(result);
+
+var fullName = `${firstName} ${lastName}`;
+
+console.log(fullName);
